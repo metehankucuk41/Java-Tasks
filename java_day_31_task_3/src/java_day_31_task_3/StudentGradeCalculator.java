9379eab814cf2769b3b3d05b16a4c7c2e243f826
metehankucuk41 <metehankkucuk41@gmail.com>
2024-08-05 23:22:59 +0300

    V.1.0

--- java_day_31_task_3/src/java_day_31_task_3/StudentGradeCalculator.java
+++ java_day_31_task_3/src/java_day_31_task_3/StudentGradeCalculator.java
@@ -1,135 +1,147 @@
 package java_day_31_task_3;
+
 import java.util.Random;
 import java.util.Scanner;
 
 class StudentGradeCalculator 
 {
-	
-	int numberOfStudents, numberOfCourses;
-	
-	boolean studentNamesAuto, courseNamesAuto;
-	
-	String[] defaultCourseNames = {"Algorithm and Programming", "Maths", "Physical Science", "Chemistry", "Biology", "English", "History"};
-	String[] defaultStudentNames = {"Adam", "Joe", "Katty", "William", "Mary", "Jennifer", "Paticia"};
-	
-	String[] courseNames, studentNames;
-	
-	int[][] grades;
-	
-	
-	Random random = new Random();
-	Scanner scanner = new Scanner(System.in);
-	
-	public void createArrays()
-	{
-		if (numberOfStudents == -1)
-		{
-			numberOfStudents = random.nextInt(2, 7);
-		}
-		
-		if (numberOfCourses == -1)
-		{
-			numberOfCourses = random.nextInt(2, 7);
-		}
-		
-		courseNames = new String[numberOfCourses];
-		studentNames = new String[numberOfStudents];
-		
-		grades = new int[studentNames.length][courseNames.length];
-		
-		if (studentNamesAuto)
-		{
-			for (int i = 0; i < studentNames.length; i++)
-			{
-				studentNames[i] = defaultStudentNames[i % defaultStudentNames.length];
-			}
-		}
-		else
-		{
-			for (int i = 0; i < studentNames.length; i++)
-			{
-				System.out.print("Enter the name of student " + (i+1) + " name: ");
-				studentNames[i] = scanner.nextLine();
-			}
-		}
-		
-		
-		
-		if (courseNamesAuto)
-		{
-			for (int i = 0; i < studentNames.length; i++)
-			{
-				courseNames[i] = defaultCourseNames[i % defaultCourseNames.length];
-			}
-		}
-		
-		else
-		{
-			for (int i = 0; i < courseNames.length; i++)
-			{
-				System.out.print("Enter the name of course " + (i+1) + " name: ");
-				courseNames[i] = scanner.nextLine();
-			}
-		}
-		
-		for (int i = 0; i < studentNames.length; i++)
-		{
-			for (int j = 0; j < courseNames.length; j++)
-			{
-				grades[i][j] = random.nextInt(101);
-			}
-		}
-		
-		
-	}
-	
-	
-	public int AnyStudentsGrade(String studentName)
-	{
-		int studentIndex = findStudentIndex(studentName);
-		
-		if (studentIndex >= 0 || studentIndex < studentNames.length)
-		{
-			int sum = 0;
-			for (int i = 0; i < courseNames.length; i++)
-			{
-				sum += grades[studentIndex][i];
-			}
-			return sum / courseNames.length;
-		}
-		else
-		{
-			System.out.println("The student you were looking for was not found.");
-		}
-		return -1;
-	}
-	
-	public int AnyCourseAverageGrade(String courseName)
-	{
-		return AnyCourseTotalGrade(courseName) / studentNames.length;
-	}
-	
-	public int AnyCourseTotalGrade(String courseName)
-	{
-		int sum = 0;
-		if (findCourseIndex(courseName) == -1)
-		{
-			System.out.print("Please enter a valid course name!");
-		}
-		else
-		{
-			for (int i = 0; i < studentNames.length; i++)
-			{
-				sum += grades[i][findCourseIndex(courseName)];
-			}
-		}
-		return sum;
-	}
-	
-	
-	public int AnyStudentsAnyCourseGrade(String studentName, String courseName)
-	{
-		int studentIndex = findStudentIndex(studentName);
+    int numberOfStudents, numberOfCourses;
+    
+    boolean studentNamesAuto, courseNamesAuto;
+    
+    String[] defaultCourseNames = {"Algorithm and Programming", "Maths", "Physical Science", "Chemistry", "Biology", "English", "History"};
+    String[] defaultStudentNames = {"Adam", "Joe", "Katty", "William", "Mary", "Jennifer", "Paticia"};
+    
+    String[] courseNames, studentNames;
+    
+    int[][] grades;
+    
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+    
+    public void initialize(int numberOfStudents, int numberOfCourses, boolean studentNamesAuto, boolean courseNamesAuto) 
+    {
+    	
+        this.numberOfStudents = (numberOfStudents == -1) ? random.nextInt(2, 7) : numberOfStudents;
+        this.numberOfCourses = (numberOfCourses == -1) ? random.nextInt(2, 7) : numberOfCourses;
+        
+        this.studentNamesAuto = studentNamesAuto;
+        this.courseNamesAuto = courseNamesAuto;
+        
+        createArrays();
+    }
+    
+    private void createArrays()
+    {
+        courseNames = new String[numberOfCourses];
+        studentNames = new String[numberOfStudents];
+        
+        grades = new int[studentNames.length][courseNames.length];
+        
+        if (studentNamesAuto)
+        {
+            for (int i = 0; i < studentNames.length; i++)
+            {
+                studentNames[i] = defaultStudentNames[i % defaultStudentNames.length];
+            }
+        }
+        
+        else
+        {
+            for (int i = 0; i < studentNames.length; i++)
+            {
+                System.out.print("Enter the name of student " + (i+1) + ": ");
+                studentNames[i] = scanner.nextLine();
+            }
+        }
+        
+        if (courseNamesAuto)
+        {
+            for (int i = 0; i < courseNames.length; i++)
+            {
+                courseNames[i] = defaultCourseNames[i % defaultCourseNames.length];
+            }
+        }
+        
+        else
+        {
+            for (int i = 0; i < courseNames.length; i++)
+            {
+                System.out.print("Enter the name of course " + (i+1) + ": ");
+                courseNames[i] = scanner.nextLine();
+            }
+        }
+        
+        for (int i = 0; i < studentNames.length; i++)
+        {
+            for (int j = 0; j < courseNames.length; j++)
+            {
+                grades[i][j] = random.nextInt(101);
+            }
+        }
+    }
+    
+    public int AnyStudentsGrade(String studentName)
+    {
+        int studentIndex = findStudentIndex(studentName);
+        
+        if (studentIndex != -1)
+        {
+            int sum = 0;
+            for (int i = 0; i < courseNames.length; i++)
+            {
+                sum += grades[studentIndex][i];
+            }
+            return sum / courseNames.length;
+        }
+        
+        else
+        {
+            System.out.println("The student you were looking for was not found.");
+            return -1;
+        }
+    }
+    
+    public int AnyCourseAverageGrade(String courseName)
+    {
+        int courseIndex = findCourseIndex(courseName);
+        
+        if (courseIndex != -1)
+        {
+            return AnyCourseTotalGrade(courseName) / studentNames.length;
+        }
+        
+        else
+        {
+            System.out.println("Please enter a valid course name!");
+            return -1;
+        }
+    }
+    
+    public int AnyCourseTotalGrade(String courseName)
+    {
+        int courseIndex = findCourseIndex(courseName);
+        
+        if (courseIndex != -1)
+        {
+            int sum = 0;
+            for (int i = 0; i < studentNames.length; i++)
+            {
+                sum += grades[i][courseIndex];
+            }
+            return sum;
+        }
+        else
+        {
+            System.out.println("Please enter a valid course name!");
+            return -1;
+        }
+    }
+    
+    public int AnyStudentsAnyCourseGrade(String studentName, String courseName)
+    {
+    	
+        int studentIndex = findStudentIndex(studentName);
         int courseIndex = findCourseIndex(courseName);
         
         if (studentIndex != -1 && courseIndex != -1) 
@@ -141,39 +153,38 @@ class StudentGradeCalculator
             System.out.println("Invalid student or course name.");
             return -1;
         }
-	}
+    }
     
-	public int findStudentIndex(String studentName)
-	{
-		for (int i = 0; i < studentNames.length; i++)
-		{
-			if (studentNames[i].equalsIgnoreCase(studentName))
-			{
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	
-	public int findCourseIndex(String courseName)
-	{
-		for (int i = 0; i < courseNames.length; i++)
-		{
-			if (courseNames[i].equalsIgnoreCase(courseName))
-			{
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	public void showArrays()
-	{
-		System.out.print("\t");
+    private int findStudentIndex(String studentName)
+    {
+        for (int i = 0; i < studentNames.length; i++)
+        {
+            if (studentNames[i].equalsIgnoreCase(studentName))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private int findCourseIndex(String courseName)
+    {
         for (int i = 0; i < courseNames.length; i++)
         {
-            System.out.print(courseNames[i] + "\t");
+            if (courseNames[i].equalsIgnoreCase(courseName))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public void showArrays()
+    {
+        System.out.print("\t");
+        for (String courseName : courseNames)
+        {
+            System.out.print(courseName + "\t");
         }
         System.out.println();
         
@@ -186,5 +197,5 @@ class StudentGradeCalculator
             }
             System.out.println();
         }
-	}
+    }
 }
--- java_day_31_task_3/src/java_day_31_task_3/StudentGradeCalculatorClient.java
+++ java_day_31_task_3/src/java_day_31_task_3/StudentGradeCalculatorClient.java
@@ -1,123 +1,88 @@
 package java_day_31_task_3;
+
 import java.util.Scanner;
 
 public class StudentGradeCalculatorClient 
 {
-
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
-        //Random random = new Random();
         StudentGradeCalculator calculator = new StudentGradeCalculator();
         
         System.out.print("Please enter the number of students. (Press -1 for it to be created automatically.): ");
-        int numberOfStudent = scanner.nextInt();
+        int numberOfStudents = scanner.nextInt();
         
         System.out.print("Please enter the number of courses. (Press -1 for it to be created automatically.): ");
         int numberOfCourses = scanner.nextInt();
         
-        boolean studentNamesAuto = true, courseNamesAuto = true;
-        
-        
-        System.out.print("Will you enter student names manually?: ");
         scanner.nextLine();
-        String isManuelStudentNames = scanner.nextLine();
         
-        System.out.print("Will you enter course names manually?: ");
-        scanner.nextLine();
-        String isManuelCourseNames = scanner.nextLine();
+        System.out.print("Will you enter student names manually? (yes/no): ");
+        boolean studentNamesAuto = !scanner.nextLine().equalsIgnoreCase("yes");
         
-        if (isManuelStudentNames.toLowerCase().contains("yes"))
-        {
-        	studentNamesAuto = false;
-        }
-        if (isManuelCourseNames.toLowerCase().contains("no"))
-        {
-        	courseNamesAuto = false;
-        }
+        System.out.print("Will you enter course names manually? (yes/no): ");
+        boolean courseNamesAuto = !scanner.nextLine().equalsIgnoreCase("yes");
         
-        calculator.numberOfCourses = numberOfCourses;
-        calculator.numberOfStudents = numberOfStudent;
-        calculator.studentNamesAuto = studentNamesAuto;
-        calculator.courseNamesAuto = courseNamesAuto;
-        calculator.createArrays();
+        calculator.initialize(numberOfStudents, numberOfCourses, studentNamesAuto, courseNamesAuto);
         calculator.showArrays();
         
         boolean control = true;
         while (control)
         {
-        	transactionMenu();
-        	int transaction = scanner.nextInt();
-        	String studentName, courseName;
-        	switch(transaction)
-        	{
-        	
-        		case 1:
-        			
-        			System.out.print("Type which student's GPA you want to know: ");
-        			scanner.nextLine();
-        			studentName = scanner.nextLine();
-        			System.out.println(studentName + "'s GPA: " + calculator.AnyStudentsGrade(studentName));
-        			break;
-        			
-        		case 2:
-        			
-        			System.out.print("Type the GPA of which course you want to know: ");
-        			scanner.nextLine();
-        			courseName = scanner.nextLine();
-        			
-        			System.out.println("GPA of the " + courseName + " course: " + calculator.AnyCourseAverageGrade(courseName));
-        			calculator.AnyCourseAverageGrade(courseName);
-        			
-        			break;
-        			
-        		case 3:
-        			
-        			System.out.print("Type the grade total for which course you want to know: ");
-        			scanner.nextLine();
-        			courseName = scanner.nextLine();
-        			System.out.println("Total grade of the " + courseName + " course: " + calculator.AnyCourseTotalGrade(courseName));
-        			
-        			break;
-        			
-        		case 4:
-        			
-        			System.out.print("Enter the course name: ");
-        			scanner.nextLine();
-        			courseName = scanner.nextLine();
-        			
-        			System.out.print("Enter the student name: ");
-        			scanner.nextLine();
-        			studentName = scanner.nextLine();
-        			
-        			System.out.println(studentName + "'s " + courseName + " course grade: " + calculator.AnyStudentsAnyCourseGrade(studentName, courseName));
-        			
-        			break;
-        			
-        		case 5:
-        			
-        			scanner.close();
-        			control = false;
-        			break;
-        			
-        		default:
-        			
-        			System.out.println("Please enter a valid transaction!");
-        			break;
-        	}
+            transactionMenu();
+            int transaction = scanner.nextInt();
+            scanner.nextLine();
+            String studentName, courseName;
+            switch(transaction)
+            {
+                case 1:
+                    System.out.print("Type which student's GPA you want to know: ");
+                    studentName = scanner.nextLine();
+                    System.out.println(studentName + "'s GPA: " + calculator.AnyStudentsGrade(studentName));
+                    break;
+                    
+                case 2:
+                    System.out.print("Type the GPA of which course you want to know: ");
+                    courseName = scanner.nextLine();
+                    System.out.println("GPA of the " + courseName + " course: " + calculator.AnyCourseAverageGrade(courseName));
+                    break;
+                    
+                case 3:
+                    System.out.print("Type the grade total for which course you want to know: ");
+                    courseName = scanner.nextLine();
+                    System.out.println("Total grade of the " + courseName + " course: " + calculator.AnyCourseTotalGrade(courseName));
+                    break;
+                    
+                case 4:
+                    System.out.print("Enter the course name: ");
+                    courseName = scanner.nextLine();
+                    
+                    System.out.print("Enter the student name: ");
+                    studentName = scanner.nextLine();
+                    
+                    System.out.println(studentName + "'s " + courseName + " course grade: " + calculator.AnyStudentsAnyCourseGrade(studentName, courseName));
+                    break;
+                    
+                case 5:
+                    scanner.close();
+                    control = false;
+                    break;
+                    
+                default:
+                    System.out.println("Please enter a valid transaction!");
+                    break;
+            }
         }
     }
     
     public static void transactionMenu()
     {
-    	System.out.println();
-    	System.out.println("To learn a student's overall grade point average, press 1.");
+        System.out.println();
+        System.out.println("To learn a student's overall grade point average, press 1.");
         System.out.println("To learn the overall grade point average of a course, press 2.");
-        System.out.println("To learn the overall grade total for a course., press 3.");
+        System.out.println("To learn the overall grade total for a course, press 3.");
         System.out.println("To learn a student's grade in any course, press 4.");
         System.out.println("To exit, press 5.");
         System.out.print("Please enter the action you want to perform: ");
     }
-
-    
 }
